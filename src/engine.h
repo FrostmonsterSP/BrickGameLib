c95fa8f25e49d46287786ae56065f2c9981fe27c
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-18 05:57:48 +0300

    Добавление совместимости с C++

--- src/engine.h
+++ src/engine.h
@@ -20,6 +20,10 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define FIELD_HEIGHT 20  ///< Высота игрового поля в клетках
 #define FIELD_WIDTH 10  ///< Ширина игрового поля в клетках
 
@@ -181,5 +185,9 @@ void userInput(UserAction_t action);
  * состоянии игры.
  */
 const GameInfo_t *updateCurrentState(void);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif  // ENGINE_H
